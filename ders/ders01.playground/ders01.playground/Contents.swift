80ed28f904a3d973f4e6ac9307f7f2b0b77e83b3
ali kolukirik <aakolukirik@gmail.com>
2021-04-25 23:54:53 +0200

    Add files via upload

---
+++ ders/ders01.playground/ders01.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+
+// toggle fonksiyonu tersine cevirme islemi yapar
+
+var a = 1<2
+a.toggle()
+a.toggle()
+
+
+Int8.min
+Int8.max
+
+UInt8.min
+UInt8.max
+
+Int64.min
+Int64.max
+
+UInt64.min
+UInt64.max
+
+Double.leastNormalMagnitude
+Double.greatestFiniteMagnitude
+
+Float.leastNormalMagnitude
+Float.greatestFiniteMagnitude
+
+/**
+    Takma ad / Type aliases
+*/
+
+typealias Animal = String
+
+
+let myPet: Animal = "Dog"
+
+
+
+
+
+
+
